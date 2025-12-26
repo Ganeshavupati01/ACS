fe21c8bf286a4ca450ec2a71a962ce3680f8b4db
Avupati Ganesh <161594924+Ganeshavupati01@users.noreply.github.com>
2025-12-26 17:29:12 +0530

    Add header to Blogs component

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,6 +1,7 @@
 export default function Blogs() {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
+      <h2>hiiii</h2>
       <iframe
         src="https://blog-ssr-phi.vercel.app/blogs"
         title="Blogs"
