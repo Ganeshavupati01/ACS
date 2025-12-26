7323158a1c6999c69e96547df3d3d086acc176ec
Avupati Ganesh <161594924+Ganeshavupati01@users.noreply.github.com>
2025-12-26 17:57:39 +0530

    Increase width of Blogs component and iframe

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,12 +1,12 @@
 export default function Blogs() {
   return (
-    <div style={{ height: "100vh", width: "100%" }}>
+    <div style={{ height: "100vh", width: "200%" }}>
       <h2>hiiii</h2>
       <iframe
         src="https://blog-ssr-phi.vercel.app/blogs"
         title="Blogs"
         style={{
-          width: "100%",
+          width: "200%",
           height: "100%",
           border: "none",
         }}
